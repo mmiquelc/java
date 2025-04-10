6dd5ef52f9d9c20f5848baeccc3147ffca8e625d
marc <2005marcmc@gmail.com>
2025-04-10 17:52:26 +0200

    Eliminar arxius .metadata

--- ExercicisClaseTascas_6_7_9/src/Tasca9_TipusDadesCompostes_TractamentCadenas/Cadenes_6.java
+++ ExercicisClaseTascas_6_7_9/src/Tasca9_TipusDadesCompostes_TractamentCadenas/Cadenes_6.java
@@ -1,6 +1,6 @@
-package matrius;
+package Tasca9_TipusDadesCompostes_TractamentCadenas;
 
-public class SelectionSort {
+public class Cadenes_6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
