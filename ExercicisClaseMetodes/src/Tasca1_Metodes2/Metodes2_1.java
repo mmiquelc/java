ad571f98d58c8c14355a404d1e446d3d2ae5b56a
marc <2005marcmc@gmail.com>
2025-04-11 17:14:55 +0200

    jhiwjbgwib

---
+++ ExercicisClaseMetodes/src/Tasca1_Metodes2/Metodes2_1.java
@@ -0,0 +1,30 @@
+package Tasca1_Metodes2;
+
+import java.util.Scanner;
+
+public class Metodes2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner (System.in);
+		
+		System.out.println("Escriu una paraula: ");
+		String word = in.nextLine();
+		
+		
+	}
+
+	
+	public static String barrejarParaula(String paraula) {
+		
+		
+		
+		
+		
+		
+		System.out.println(word.substring(0, word.length()-1));
+		
+	
+	}
+}
